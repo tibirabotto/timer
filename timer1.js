b26185ceb3b98622dc390d458fcbec65e77ba0e2
tibirabotto <tibira.botto@gmail.com>
2023-10-24 14:26:31 -0400

    timer1

---
+++ timer1.js
@@ -0,0 +1,24 @@
+process.stdout.write('\x07');
+const parms = Array.from(process.argv.splice(2)).sort((a, b) => a - b);
+const childProcess  = require('child_process');
+
+const timer = (numbers) => {
+	let timer = Number(numbers[0]) + 1000;
+	let index = 0;
+	const counter = () => {
+		for (const element of numbers) {
+			if (index < numbers.length) {
+				process.stdout.write('\x07');
+				index++
+				setTimeout(counter, timer)
+				timer = Number(element) + 1000
+			}
+		}
+	}
+
+	setTimeout(counter, timer)
+
+}
+
+timer(parms)
+
